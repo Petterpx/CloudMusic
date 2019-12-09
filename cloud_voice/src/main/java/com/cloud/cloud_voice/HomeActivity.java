e136543e0924df9be25930353bb7afc9a28d2728
Petterp <1509492795@qq.com>
2019-12-09 22:00:41 +0800

    初始化项目组件

---
+++ cloud_voice/src/main/java/com/cloud/cloud_voice/HomeActivity.java
@@ -0,0 +1,27 @@
+package com.cloud.cloud_voice;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.FragmentActivity;
+
+/**
+ * Created by Petterp
+ * on 2019-12-09
+ * Function:
+ */
+public class HomeActivity extends FragmentActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+    private void initData(){
+
+    }
+
+    private void initView(){
+
+    }
+}
